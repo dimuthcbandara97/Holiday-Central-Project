15c13ab0c89648ac33737ee368b40c1d51f5bf7f
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-05-04 12:38:57 +0530

    Cart Update Added

--- frontend/holidaycentral/src/Components/Cart.js
+++ frontend/holidaycentral/src/Components/Cart.js
@@ -1,37 +1,191 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import HeaderAll from "./Headers/HeaderAll";
+import axios from "axios";
 
 const Cart = () => {
+  const [dataFlight, setDataFlight] = useState([]);
+  const [dataHotel, setDataHotel] = useState([]);
+  const [dataPackage, setDataPackage] = useState([]);
+
+  useEffect(() => {
+    // Cart Flight
+    axios
+      .get("http://localhost:4000/api/cart/flight")
+      .then((response) => {
+        setDataFlight(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // Cart Hotel
+    axios
+      .get("http://localhost:4000/api/cart/hotel")
+      .then((response) => {
+        setDataHotel(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // // Cart Package
+    axios
+      .get("http://localhost:4000/api/checkout/package")
+      .then((response) => {
+        setDataPackage(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
       <HeaderAll />
+
       <div class="container m-3 ps-3">
         <div class="display-4 fw-bold text-dark">
-          <i class="bi bi-bag-check me-4"></i>Cart
+          <i class="bi bi-calendar-check me-4"></i>View My Cart
         </div>
         <div class="container border rounded mt-5 p-3 fw-bold">
-          <table class="table">
+          <div class="display-6">Flight Cart</div>
+          <table class="table table-bordered border-primary text-center">
             <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Hotel Name</th>
+              <tr class="table-success">
+                <th scope="col">Agent Name</th>
+                <th scope="col">User Name</th>
+                <th scope="col">User Email</th>
+                <th scope="col">Bill Date</th>
+                <th scope="col">Additional Note</th>
+                <th scope="col">Departure Destination</th>
+                <th scope="col">Arrival Destination</th>
+                <th scope="col">Departure Date</th>
+                <th scope="col">Arrival Date</th>
+                <th scope="col">Cabin Class</th>
                 <th scope="col">Price</th>
-                <th scope="col">Star Rating</th>
                 <th scope="col">Duration</th>
-                <th scope="col"></th>
+                <th scope="col">Airline</th>
+                <th scope="col">Indian Food</th>
+                <th scope="col">Sri Lankan Food</th>
+                <th scope="col">Italian Food</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {dataFlight.map((item) => (
+                <tr key={item.id}>
+                  <td> {item.agent_name}</td>
+                  <td> {item.user_name}</td>
+                  <td> {item.user_email}</td>
+                  <td> {item.checkout_date}</td>
+                  <td> {item.additional_note}</td>
+                  <td> {item.departure_destination}</td>
+                  <td> {item.arrival_destination}</td>
+                  <td> {item.departure_date}</td>
+                  <td> {item.arrival_date}</td>
+                  <td> {item.cabin_class}</td>
+                  <td> {item.price}</td>
+                  <td> {item.duration} Days</td>
+                  <td> {item.airline}</td>
+                  <td>{item.meal_preferences.indian}</td>
+                  <td> {item.meal_preferences.sri_lankan}</td>
+                  <td>{item.meal_preferences.italian}</td>
+                </tr>
+              ))}
+
+              {dataFlight.map((item) => (
+                <div key={item.id}>
+                  <tr></tr>
+                  {/* <h3>{item.agent_name}</h3>
+          <p>{item.duration ? 'Completed' : 'Not completed'}</p> */}
+                </div>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div class="container border rounded mt-5 p-3 fw-bold">
+          <div class="display-6">Hotel Cart</div>
+          <table class="table table-bordered border-primary text-center">
+            <thead>
+              <tr class="table-success">
+                <th scope="col">Agent Name</th>
+                <th scope="col">Agent Email</th>
+                <th scope="col">Destination</th>
+                <th scope="col">Bill Date</th>
+                <th scope="col">Star Rating</th>
+                <th scope="col">Price</th>
+                <th scope="col">Deluxe Room</th>
+                <th scope="col">Super Deluxe Room</th>
+                <th scope="col">Suite</th>
+                <th scope="col">Full Board</th>
+                <th scope="col">Bread</th>
+                <th scope="col">Breakfast</th>
+                <th scope="col">Pool</th>
+                <th scope="col">Kids Play Area</th>
+                <th scope="col">Beach Access</th>
+                <th scope="col">Additional Notes</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>@mdo</td>
-                <td>
-                  <button class="btn btn-secondary">Check Out</button>
-                </td>
+              {dataHotel.map((item) => (
+                <tr key={item.id}>
+                  <td> {item.agent_name}</td>
+                  <td> {item.user_email}</td>
+                  <td> {item.destination}</td>
+                  <td> {item.check_out_date}</td>
+                  <td> {item.star_rating}</td>
+                  <td> {item.pricing}</td>
+
+                  <td>{item.room_selection.deluxe ? "Yes" : "No"} </td>
+                  <td>{item.room_selection.super_deluxe ? "Yes" : "No"}</td>
+                  <td>{item.room_selection.suite ? "Yes" : "No"} </td>
+                  <td>{item.board_basis.full_board ? "Yes" : "Not"}</td>
+                  <td> {item.bread ? "Yes" : "No"}</td>
+                  <td> {item.breakfast ? "Yes" : "No"}</td>
+                  <td>{item.facilities.pool ? "Yes" : "No"}</td>
+                  <td>{item.facilities.kids_play_area ? "Yes" : "No"}</td>
+                  <td>{item.facilities.beach_access ? "Yes" : "No"}</td>
+                  <td> {item.aditional_note}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div class="container border rounded mt-5 p-3 fw-bold">
+          <div class="display-6">Package Cart</div>
+          <table class="table table-bordered border-primary text-center">
+            <thead>
+              <tr class="table-success">
+                <th scope="col">Agent Name</th>
+                <th scope="col">Agent Email</th>
+                <th scope="col">Destination</th>
+                <th scope="col">Bill Date</th>
+                <th scope="col">Duration</th>
+                <th scope="col">Travellers Count</th>
+                <th scope="col">Price</th>
+                <th scope="col">Package Rating</th>
+                <th scope="col">Honeymoon</th>
+                <th scope="col">Beach Holiday</th>
+                <th scope="col">WildLife Excursion</th>
+                <th scope="col">Family Holiday</th>
               </tr>
+            </thead>
+            <tbody>
+              {dataPackage.map((item) => (
+                <tr key={item.id}>
+                  <td> {item.agent_name}</td>
+                  <td> {item.user_email}</td>
+                  <td> {item.destination}</td>
+                  <td> {item.checkout_date}</td>
+
+                  <td> {item.duration} days</td>
+                  <td> {item.number_of_travellers}</td>
+                  <td> {item.price}</td>
+                  <td> {item.package_rating}</td>
+                  <td> {item.speciality.honeymoon ? "Yes" : "No"}</td>
+                  <td> {item.speciality.beach_holiday ? "Yes" : "No"}</td>
+                  <td> {item.speciality.wildlife_excursion ? "Yes" : "No"}</td>
+                  <td> {item.speciality.family_holiday ? "Yes" : "No"}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
